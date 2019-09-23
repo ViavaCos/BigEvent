8a28de5435071fa82342d3da638905968e36ae67
ViavaCos <13811092694@163.com>
2019-09-23 16:56:44 +0800

    文章类型编辑功能实现

--- web_back/js/article_category.js
+++ web_back/js/article_category.js
@@ -1,3 +1,13 @@
+// 封装点击编辑的函数
+function editTr(obj) {
+    $('#tips').hide();
+    $('#category-name').val(obj.name);
+    $('#category-slug').val(obj.slug);
+
+    type.name = '编辑';
+    type.id = obj.id;
+}
+
 // 封装页面点击删除调用的函数，需要放在入口函数外，否则提示未定义
 function deleteTr(id) {
     $('#myModal').modal('show');
@@ -25,6 +35,8 @@ function categoryQuery() {
         }
     })
 }
+// 定义一个公共对象
+var type = {};
 
 $(function () {
     // 页面载入后获取服务器类别
@@ -33,6 +45,11 @@ $(function () {
     // 点击新增分类清空表单内数据
     $('#addNew').click(function () {
         $('#tips').hide();
+        $('#category-name').val('');
+        $('#category-slug').val('')
+        $('#addModal .modal-title').html('新增分类名称');
+        $('#addModal #model_add').html('新增');
+        type.name = '新增';
     })
 
     // 设置新增类别功能
@@ -43,17 +60,30 @@ $(function () {
 
         // console.log(name, slug);
 
-        category.add(name, slug, function (res) {
-            // console.log(res);
-            if (res.code === 200) {
-                categoryQuery();
-                $('#addModal').modal('hide');
-                $('#category-name').val('');
-                $('#category-slug').val('')
-            } else {
-                $('#tips').show().text(res.msg);
-            }
-        })
+        if (type.name === '新增') {
+            category.add(name, slug, function (res) {
+                // console.log(res);
+                if (res.code === 200) {
+                    categoryQuery();
+                    $('#addModal').modal('hide');
+                    $('#category-name').val('');
+                    $('#category-slug').val('')
+                } else {
+                    $('#tips').show().text(res.msg);
+                }
+            })
+        } else {
+            category.edit(type.id, name, slug, function (res) {
+                // console.log(res);
+                if (res.code === 200) {
+                    categoryQuery();
+                    $('#addModal').modal('hide');
+                } else {
+                    $('#tips').show().text(res.msg);
+                }
+            })
+
+        }
     })
 
 
--- web_back/js/utils/category.js
+++ web_back/js/utils/category.js
@@ -1,10 +1,20 @@
 var category = {
+    /**
+     * 类别查询功能
+     * @param {*} callBack 回调函数 
+     */
     query: function (callBack) {
         $.get(URLList.category_search, function (res) {
             callBack(res);
         })
     },
 
+    /**
+     * 添加类别功能
+     * @param {*} name  类别名称
+     * @param {*} slug  别名
+     * @param {*} callBack  回调函数
+     */
     add: function (name, slug, callBack) {
         $.post(URLList.category_add, {
             name: name,
@@ -14,9 +24,31 @@ var category = {
         })
     },
 
+    /**
+     * 删除类别功能
+     * @param {*} id 文章id
+     * @param {*} callBack  回调函数
+     */
     delete: function (id, callBack) {
         $.post(URLList.category_delete, {
             id: id
+        }, function (res) {
+            callBack(res);
+        })
+    },
+
+    /**
+     * 类别编辑功能
+     * @param {*} id 文章id
+     * @param {*} name  类别名称
+     * @param {*} slug  别名
+     * @param {*} callBack  回调函数
+     */
+    edit: function (id, name, slug, callBack) {
+        $.post(URLList.category_edit, {
+            id: id,
+            name: name,
+            slug: slug
         }, function (res) {
             callBack(res);
         })
